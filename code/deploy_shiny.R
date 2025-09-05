5a62948ea1ff5b5ac745f0cda9ca539d1a5f071b
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2025-09-05 14:47:05 +0200

    Update deploy_shiny.R

--- code/deploy_shiny.R
+++ code/deploy_shiny.R
@@ -1,5 +1,5 @@
-install.packages(c("rsconnect", "shiny", "shinyhelper", "remotes", "plotly", "zoo", "httr", "magrittr",
-                   "shinybusy"), dependencies = NA)
+# install.packages(c("rsconnect", "shiny", "shinyhelper", "remotes", "plotly", "zoo", "httr", "magrittr",
+#                    "shinybusy"), dependencies = NA)
 
 #library("remotes")
 #remotes::install_version("rsconnect", "0.8.29")
@@ -7,20 +7,21 @@ install.packages(c("rsconnect", "shiny", "shinyhelper", "remotes", "plotly", "zo
 # install.packages("devtools")
 # devtools::install_github("rstudio/rsconnect")
 
-library("shiny")
-library("shinyhelper")
-library("rsconnect")
-library("plotly")
-library("zoo")
-library("httr")
-library("magrittr")
-library("shinybusy")
+# library("shiny")
+# library("shinyhelper")
+# library("rsconnect")
+# library("plotly")
+# library("zoo")
+# library("httr")
+# library("magrittr")
+# library("shinybusy")
 # library("DT")
 
+install.packages(c("rsconnect"), dependencies = NA)
 
 SHINYAPPS_NAME = Sys.getenv("SHINYAPPS_NAME")
 SHINYAPPS_TOKEN = Sys.getenv("SHINYAPPS_TOKEN")
 SHINYAPPS_SECRET = Sys.getenv("SHINYAPPS_SECRET")
 
 setAccountInfo(name=SHINYAPPS_NAME, token=SHINYAPPS_TOKEN, secret=SHINYAPPS_SECRET)
-deployApp(appDir = "./respinow_viz", appName = "respinow_viz")
+deployApp(appDir = "./respinow_viz", appName = "respinow_viz", forceUpdate = TRUE)
